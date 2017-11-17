e0da73d8f810d5d4b7918cfbe81b3d7814aa1a64
wanderingslc <stan.martineau@gmail.com>
2017-11-16 19:56:07 -0700

    Add smart listing to import page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,12 +6,13 @@ class LocationsController < ApplicationController
   end
 
   def new
+    @locations = smart_listing_create(:locations, Location.all, partial: "locations/listing")
   end
 
   def import
     Location.import(params[:file])
 
-    redirect_to root_url, notice: 'Locations imported'
+    redirect_to locations_new_path, notice: 'Locations imported'
   end
 
 
